eb3cdb2b90b4559cca7457b8120abd02c8666d45
Keshav Priyadarshi <keshavpriyadarshi93@gmail.com>
2021-07-23 00:04:22 +0530

    integrate pincode api & autofill city & state

--- src/container/CheckoutAddress.js
+++ src/container/CheckoutAddress.js
@@ -13,6 +13,7 @@ const CheckoutAddress = ({ history }) => {
     const { authCartItems } = useSelector(state => state.AuthCart)
 
     const [address, setAddress] = useState({})
+    const [pincodeError, setPincodeError] = useState('')
 
 
     useEffect(() => {
@@ -34,7 +35,26 @@ const CheckoutAddress = ({ history }) => {
     }
 
     const handleChange = (e) => {
+    }
+    const pincodeHandler = (e) => {
         setAddress(address => ({ ...address, [e.target.name]: e.target.value }))
+
+        fetch(`https://api.postalpincode.in/pincode/${e.target.value}`)
+            .then(data => data.json())
+            .then(data => {
+                if (data[0].Status === "Success") {
+                    setPincodeError('')
+                    setAddress(address => ({
+                        ...address,
+                        city: data[0].PostOffice && data[0].PostOffice[0].District,
+                        state: data[0].PostOffice && data[0].PostOffice[0].State
+                    })
+                    )
+                }
+                if (data[0].Status === "Error") {
+                    setPincodeError('Not a valid pincode')
+                }
+            })
     }
 
     return (
@@ -53,21 +73,49 @@ const CheckoutAddress = ({ history }) => {
                         <Form onSubmit={submitHandler}>
 
                             <Row>
-                                <Col><Form.Group><Form.Control required defaultValue={address.name} name='name' placeholder='Name' type='text' onChange={handleChange} /></Form.Group></Col>
-                                <Col><Form.Group><Form.Control required defaultValue={address.mobile} name='mobile' placeholder='10-digit mobile number' type='number' onChange={handleChange} /></Form.Group></Col>
-                            </Row>
-                            <Row>
-                                <Col><Form.Group><Form.Control required defaultValue={address.pincode} name='pincode' placeholder='Pincode' type='number' onChange={handleChange} /></Form.Group></Col>
-                                <Col><Form.Group><Form.Control required defaultValue={address.locality} name='locality' placeholder='Locality' type='text' onChange={handleChange} /></Form.Group></Col>
-                            </Row>
-                            <Row>
                                 <Col>
-                                    <Form.Group><Form.Control required defaultValue={address.address} as='textarea' name='address' placeholder='Address(Area and Street)' onChange={handleChange} /></Form.Group>
+                                    <Form.Group>
+                                        <small>Name*</small>
+                                        <Form.Control required defaultValue={address.name} name='name' placeholder='Name' type='text' onChange={handleChange} />
+                                    </Form.Group>
+                                </Col>
+                                <Col>
+                                    <Form.Group>
+                                        <small>Phone no.*</small>
+                                        <Form.Control required defaultValue={address.mobile} name='mobile' placeholder='10-digit mobile number' type='number' onChange={handleChange} />
+                                    </Form.Group>
                                 </Col>
                             </Row>
                             <Row>
-                                <Col><Form.Group><Form.Control required defaultValue={address.city} name='city' placeholder='City' type='text' onChange={handleChange} /></Form.Group></Col>
-                                <Col><Form.Group><Form.Control required defaultValue={address.state} name='state' placeholder='State' type='text' onChange={handleChange} /></Form.Group></Col>
+                                <Col>
+                                    <Form.Group>
+                                        <small>Locality*</small>
+                                        <Form.Control required defaultValue={address.locality} name='locality' placeholder='Locality' type='text' onChange={handleChange} />
+                                    </Form.Group>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col>
+                                    <Form.Group>
+                                        <small>Address(Area and Street)*</small>
+                                        <Form.Control required defaultValue={address.address} as='textarea' name='address' placeholder='Address(Area and Street)' onChange={handleChange} />
+                                    </Form.Group>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col>
+                                    <Form.Group>
+                                        <small>Pincode*</small>
+                                        <Form.Control required defaultValue={address.pincode} name='pincode' placeholder='Pincode' type='number' onChange={pincodeHandler} />
+                                        <small style={{ color: 'red' }}>{pincodeError}</small>
+                                    </Form.Group>
+                                </Col>
+                                <Col><Form.Group>
+                                    <small>District*</small>
+                                    <Form.Control required defaultValue={address.city} name='city' placeholder='City' type='text' onChange={handleChange} disabled /></Form.Group></Col>
+                                <Col><Form.Group>
+                                    <small>State*</small>
+                                    <Form.Control required defaultValue={address.state} name='state' placeholder='State' type='text' onChange={handleChange} disabled /></Form.Group></Col>
                             </Row>
 
                             <button className='page_btn' >Save and Proceed</button>
